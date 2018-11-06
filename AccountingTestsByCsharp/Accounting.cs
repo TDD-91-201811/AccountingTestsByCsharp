4202011cc9921b71ed5712461a2ca9da7013cffc
Joey Chen <jay.hatelove@gmail.com>
2018-11-07 06:49:55 +0800

    extract method: CreatePeriod()

--- AccountingTestsByCsharp/Accounting.cs
+++ AccountingTestsByCsharp/Accounting.cs
@@ -22,11 +22,16 @@ namespace AccountingTestsByCsharp
                 var period = new Period(start, end);
 
                 var dailyAmount = budget.Amount / budget.Days;
-                var overlappingDays = period.OverlappingDays(new Period(budget.FirstDay, budget.LastDay));
+                var overlappingDays = period.OverlappingDays(CreatePeriod(budget));
                 return (decimal)(dailyAmount * overlappingDays);
             }
 
             return 0;
+        }
+
+        private static Period CreatePeriod(Budget budget)
+        {
+            return new Period(budget.FirstDay, budget.LastDay);
         }
     }
 }
