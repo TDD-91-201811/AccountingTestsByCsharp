f60bee05504e134cd03bd91bf00b68e1ca6130c6
Joey Chen <jay.hatelove@gmail.com>
2018-11-07 05:44:50 +0800

    add failed test: no budgets

---
+++ AccountingTestsByCsharp/Accounting.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AccountingTestsByCsharp
+{
+    public class Accounting
+    {
+        public decimal TotalAmount(DateTime start, DateTime end)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- AccountingTestsByCsharp/UnitTest1.cs
+++ AccountingTestsByCsharp/UnitTest1.cs
@@ -1,14 +1,17 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace AccountingTestsByCsharp
 {
     [TestClass]
-    public class UnitTest1
+    public class AccountingTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void no_budgets()
         {
+            var accounting = new Accounting();
+            var totalAmount = accounting.TotalAmount(new DateTime(2010, 4, 1), new DateTime(2010, 4, 1));
+            Assert.AreEqual(0, totalAmount);
         }
     }
 }
